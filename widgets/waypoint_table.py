b48837af1bb9f4ddf1f6357f23e0cc1238dd83ed
Chris Bonomi <bonomi.c11@gmail.com>
2022-06-25 23:56:47 -0400

    Table is lookin pretty good

--- widgets/waypoint_table.py
+++ widgets/waypoint_table.py
@@ -9,7 +9,9 @@ class WaypointTable(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
+        # self.setMinimumSize(300, 500)
         self.setMinimumWidth(500)
+        # self.setMinimumHeight(300)
 
         self.heading_layout = QtWidgets.QHBoxLayout()
         x_label = QtWidgets.QLabel('X')
@@ -25,37 +27,15 @@ class WaypointTable(QtWidgets.QWidget):
         self.heading_layout.addWidget(delete_label)
 
         self.scroll_area = QtWidgets.QScrollArea()
+        self.scroll_area.setWidgetResizable(True)
         self.tableBody = WaypointTableBody()
         self.scroll_area.setWidget(self.tableBody)
 
         self.main_layout = QtWidgets.QVBoxLayout()
         self.main_layout.addLayout(self.heading_layout)
-        self.main_layout.addWidget(self.tableBody)
+        self.main_layout.addWidget(self.scroll_area)
 
         self.setLayout(self.main_layout)
-
-
-        # self.waypoints = []
-
-        # self.grid_layout = QtWidgets.QGridLayout()
-        # self.grid_layout.setAlignment(QtCore.Qt.AlignTop)
-        # self.create_table_heading()
-
-        # self.setLayout(self.grid_layout)
-
-    def create_table_heading(self):
-        print('creating heading')
-        x_label = QtWidgets.QLabel('X')
-        y_label = QtWidgets.QLabel('Y')
-        heading_label = QtWidgets.QLabel('Heading')
-        enabled_label = QtWidgets.QLabel('Enabled')
-        delete_label = QtWidgets.QLabel('Delete')
-
-        self.grid_layout.addWidget(x_label, 0, 0)
-        self.grid_layout.addWidget(y_label, 0, 1)
-        self.grid_layout.addWidget(heading_label, 0, 2)
-        self.grid_layout.addWidget(enabled_label, 0, 3)
-        self.grid_layout.addWidget(delete_label, 0, 4)
 
     def update(self):
         self.waypoints = []
@@ -69,42 +49,7 @@ class WaypointTable(QtWidgets.QWidget):
             self.waypoints.append(Waypoint(x_val, y_val, heading_val, enabled_val))
 
     def get_waypoints(self) -> list[Waypoint]:
-        self.update()
-        return self.waypoints
+        return self.tableBody.get_waypoints()
 
     def add_waypoint(self, wp: Waypoint):
-        self.tableBody.add_waypoint(Waypoint(0, 0, 0))
-
-        # numRows = self.grid_layout.rowCount()
-
-        # x_input = QtWidgets.QLineEdit(str(wp.x))
-        # y_input = QtWidgets.QLineEdit(str(wp.y))
-        # heading_input = QtWidgets.QLineEdit(str(wp.heading))
-        # enabled_input = QtWidgets.QCheckBox()
-        # enabled_input.setChecked(wp.enabled)
-        # delete_input = QtWidgets.QPushButton('X')
-        # delete_input.clicked.connect(lambda: self.delete_row(numRows))
-
-        # self.grid_layout.addWidget(x_input, numRows, 0)
-        # self.grid_layout.addWidget(y_input, numRows, 1)
-        # self.grid_layout.addWidget(heading_input, numRows, 2)
-        # self.grid_layout.addWidget(enabled_input, numRows, 3)
-        # self.grid_layout.addWidget(delete_input, numRows, 4)
-
-    def delete_row(self, rowNum):
-        waypoints = self.get_waypoints()
-        del waypoints[rowNum - 1]
-
-        tempWidget = QtWidgets.QWidget()
-        tempWidget.setLayout(self.grid_layout)
-        tempWidget.deleteLater()
-
-        self.grid_layout = QtWidgets.QGridLayout()
-        self.grid_layout.setAlignment(QtCore.Qt.AlignTop)
-        self.create_table_heading()
-
-        for wp in waypoints:
-            self.add_waypoint(wp)
-
-        self.setLayout(self.grid_layout)
-        self.update()
+        self.tableBody.add_waypoint(wp)
