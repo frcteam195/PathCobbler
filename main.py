6ac63718d0a8532a8df7122c8f1bc575dac93f21
Chris Bonomi <bonomi.c11@gmail.com>
2022-06-26 00:00:41 -0400

    Allow SIGINT to end the program

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ from widgets.waypoint import Waypoint
 from widgets.waypoint_control import WaypointControl
 from widgets.waypoint_table import WaypointTable
 
+import signal
+
+
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.control = WaypointControl()
-
-        # self.control.waypoint_table.tableBody.add_waypoint(Waypoint(0, 0, 45))
-        # self.control.waypoint_table.add_waypoint(Waypoint(20, 20, 65))
 
         self.setCentralWidget(self.control)
 
