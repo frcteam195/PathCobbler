2ca4ac250e86cb17539a65171448c171641225fe
Chris Bonomi <bonomi.c11@gmail.com>
2022-06-29 23:52:25 -0400

    Always be float

--- widgets/waypoint.py
+++ widgets/waypoint.py
@@ -1,8 +1,8 @@
 class Waypoint:
     def __init__(self, x, y, heading, enabled=True):
-        self.x = x
-        self.y = y
-        self.heading = heading
+        self.x = float(x)
+        self.y = float(y)
+        self.heading = float(heading)
         self.enabled = enabled
 
     def __str__(self):
