270e6828e3bf777e12a30c1abc947168f07fdb53
Chris Bonomi <bonomi.c11@gmail.com>
2022-06-30 22:10:12 -0400

    Remove python path from build script

--- build_c.sh
+++ build_c.sh
@@ -1,5 +1,5 @@
-gcc -c -Wall -Werror -fpic lib/waypoint.c -o lib/build/waypoint.o -I /opt/homebrew/Caskroom/miniconda/base/envs/pygui/bin/python
+gcc -c -Wall -Werror -fpic lib/waypoint.c -o lib/build/waypoint.o
 gcc -shared -o lib/build/libwaypoint.so lib/build/waypoint.o
 
-g++ -c -Wall -Werror -fpic lib/waypoint.cpp -o lib/build/waypoint_cpp.o -I /opt/homebrew/Caskroom/miniconda/base/envs/pygui/bin/python
+g++ -c -Wall -Werror -fpic lib/waypoint.cpp -o lib/build/waypoint_cpp.o
 g++ -shared -o lib/build/libwaypoint_cpp.so lib/build/waypoint_cpp.o
