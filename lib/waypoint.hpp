32e6cdd4773f351069ba0a19d98ceeca4ebd10ac
Chris Bonomi <bonomi.c11@gmail.com>
2022-06-30 23:33:57 -0400

    C is still scary

--- lib/waypoint.hpp
+++ lib/waypoint.hpp
@@ -14,6 +14,10 @@ typedef struct
 
 void waypoint_test(waypoint_t wp);
 waypoint_t make_waypoint();
+waypoint_t *make_waypoints(int size);
+waypoint_t *sorcery(int *size);
+void freeme(void *ptr);
+void mod_waypoints(waypoint_t *wps, int size);
 void print_num(int x);
 
 #ifdef __cplusplus
