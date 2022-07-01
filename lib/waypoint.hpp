8f0a77bec0b71ba1660a811c79327b62b79cebd2
Chris Bonomi <bonomi.c11@gmail.com>
2022-07-01 01:20:35 -0400

    Closer to final-ish API

--- lib/waypoint.hpp
+++ lib/waypoint.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C"
 {
@@ -12,10 +14,17 @@ typedef struct
     double heading;
 } waypoint_t;
 
+typedef struct
+{
+    uint32_t size;
+    waypoint_t *wp_ptr;
+} waypoint_array_t;
+
 void waypoint_test(waypoint_t wp);
 waypoint_t make_waypoint();
 waypoint_t *make_waypoints(int size);
 waypoint_t *sorcery(int *size);
+waypoint_array_t get_waypoints();
 void freeme(void *ptr);
 void mod_waypoints(waypoint_t *wps, int size);
 void print_num(int x);
