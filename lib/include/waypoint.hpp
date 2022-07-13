da790f6379505a28e3ed6a938966f81aa2e3498f
Chris Bonomi <bonomi.c11@gmail.com>
2022-07-12 22:13:50 -0400

    Remove old functions and start implementing splines

--- lib/include/waypoint.hpp
+++ lib/include/waypoint.hpp
@@ -20,14 +20,11 @@ typedef struct
     waypoint_t *wp_ptr;
 } waypoint_array_t;
 
-void waypoint_test(waypoint_t wp);
-waypoint_t make_waypoint();
 waypoint_t *make_waypoints(int size);
-waypoint_t *sorcery(int *size);
 waypoint_array_t get_waypoints();
 void freeme(void *ptr);
-void mod_waypoints(waypoint_t *wps, int size);
-void print_num(int x);
+
+waypoint_array_t calc_splines(waypoint_array_t waypoints);
 
 #ifdef __cplusplus
 }
