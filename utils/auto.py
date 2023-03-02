35368948ba95e9a0b1f5a36ab0f0075b95a9df48
frc195programming <frc195programming@gmail.com>
2023-03-01 22:00:32 -0500

    speed update

--- utils/auto.py
+++ utils/auto.py
@@ -5,8 +5,9 @@ from utils.waypoint import Waypoint
 from typing import List
 
 class Auto(QListWidgetItem):
-    def __init__(self, name, waypoints: List[Waypoint]):
+    def __init__(self, name, waypoints: List[Waypoint], speed: float=None):
         super().__init__(name)
         self.setFlags(self.flags() | Qt.ItemFlag.ItemIsEditable)
+        self.speed = speed
         self.waypoints: List[Waypoint] = waypoints
 
